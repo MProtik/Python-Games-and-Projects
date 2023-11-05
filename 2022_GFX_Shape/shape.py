8a8d9eab53550f7492ed921edfd087c7a8aca88b
MProtik <149885997+MProtik@users.noreply.github.com>
2023-11-05 15:15:28 +0600

    Update shape.py

--- 2022_GFX_Shape/shape.py
+++ 2022_GFX_Shape/shape.py
@@ -3,15 +3,32 @@ bgcolor('black')
 speed(0)
 hideturtle()
 
-for i in range(120):
-    color('orange')
+for i in range(150):
+    color('white')
     circle(i)
-    color('red')
+    color('cyan')
     circle(i*0.8)
+    color('purple')
+    circle(i*0.6)
+    color('purple')
+    circle(i*0.55)
+    color('purple')
+    circle(i*0.5)
     right(3)
     forward(3)
 
+for i in range(150):
+    color('white')
+    circle(i)
+    color('cyan')
+    circle(i*0.8)
+    color('purple')
+    circle(i*0.6)
+    color('purple')
+    circle(i*0.55)
+    color('purple')
+    circle(i*0.5)
+    left(3)
+    forward(3)
+
 done()
-
-
-
